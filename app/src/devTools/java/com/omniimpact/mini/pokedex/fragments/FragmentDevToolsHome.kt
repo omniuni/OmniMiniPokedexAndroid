69fb22864f1ff4d10c15d7b4f236ba88906bca88
Daniel Marcus <daniel@omniimpact.com>
2024-05-07 21:47:45 -0400

    fix compilation

--- app/src/devTools/java/com/omniimpact/mini/pokedex/fragments/FragmentDevToolsHome.kt
+++ app/src/devTools/java/com/omniimpact/mini/pokedex/fragments/FragmentDevToolsHome.kt
@@ -5,8 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.omniimpact.mini.pokedex.BuildConfig
 import com.omniimpact.mini.pokedex.databinding.ActivityMainDevFragmentHomeBinding
+import com.squareup.picasso.BuildConfig
 
 class FragmentDevToolsHome : Fragment() {
 
