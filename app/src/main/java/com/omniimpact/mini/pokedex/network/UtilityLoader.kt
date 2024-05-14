ac354840dd806d44a28312a03141aa39da41cc5f
Daniel Marcus <daniel@omniimpact.com>
2024-05-14 00:25:20 -0400

    fix utility loader

--- app/src/main/java/com/omniimpact/mini/pokedex/network/UtilityLoader.kt
+++ app/src/main/java/com/omniimpact/mini/pokedex/network/UtilityLoader.kt
@@ -1,6 +1,7 @@
 package com.omniimpact.mini.pokedex.network
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import com.omniimpact.mini.pokedex.R
@@ -14,8 +15,10 @@ object UtilityLoader: IOnApiLoadProgress {
 	private lateinit var mContainer: View
 	private lateinit var mText: TextView
 
-	private val mToLoad: MutableMap<IApi, String> = mutableMapOf()
+	private val mToLoad: MutableMap<String, Pair<IApi, String>> = mutableMapOf()
 	private val mListeners: MutableList<IOnApiLoadQueue> = mutableListOf()
+
+	private var mLoadInProgress = false
 
 	fun attachOutputUi(
 		container: View,
@@ -26,11 +29,15 @@ object UtilityLoader: IOnApiLoadProgress {
 		updateComplete()
 	}
 
-	fun enqueue(list: Map<IApi, String>){
-		list.forEach { apiEntry ->
-			mToLoad[apiEntry.key] = apiEntry.value
+	fun enqueue(map: Map<IApi, String>){
+		map.forEach { apiEntry ->
+			val id = apiEntry.key.getUrl()+apiEntry.value
+			Log.d(UtilityLoader::class.simpleName, "Check ID: $id")
+			if(mToLoad.containsKey(id)) return
+			Log.d(UtilityLoader::class.simpleName, "Enqueue ID: $id")
+			mToLoad[id] = Pair(apiEntry.key, apiEntry.value)
 		}
-		continueLoading()
+		if(!mLoadInProgress) continueLoading()
 	}
 
 	fun registerListener(listener: IOnApiLoadQueue){
@@ -42,18 +49,21 @@ object UtilityLoader: IOnApiLoadProgress {
 	}
 
 	private fun continueLoading(){
+		mLoadInProgress = true
 		if(mToLoad.keys.isEmpty()){
 			updateComplete()
+			mLoadInProgress = false
 			return
 		}
-		val toLoadApi = mToLoad.keys.first()
-		val toLoadString = mToLoad[toLoadApi] ?: String()
+		val toLoadPairKey = mToLoad.keys.first()
+		Log.d(UtilityLoader::class.simpleName, "Loading: $toLoadPairKey")
+		val toLoadPair = mToLoad[toLoadPairKey] ?: return
+		toLoadPair.first.load(this, toLoadPair.second)
 		try {
 			if (this::mText.isInitialized) mText.text =
-				mText.context.getString(R.string.loading, toLoadApi.getFriendlyName())
+				mText.context.getString(R.string.loading, toLoadPair.first.getFriendlyName())
 			if (this::mContainer.isInitialized) mContainer.visibility = View.VISIBLE
 		} catch (_: Exception) {}
-		toLoadApi.load(this, toLoadString)
 	}
 
 	private fun updateComplete(){
@@ -71,7 +81,9 @@ object UtilityLoader: IOnApiLoadProgress {
 	}
 
 	override fun onSuccess(success: IApi) {
-		mToLoad.remove(success)
+		Log.d(UtilityLoader::class.simpleName, "Remove: ${success.getUrl()}")
+		mToLoad.remove(success.getUrl())
+		Log.d(UtilityLoader::class.simpleName, "Success: ${success.getUrl()}")
 		mListeners.forEach {
 			it.onSuccess(success)
 		}
@@ -79,7 +91,9 @@ object UtilityLoader: IOnApiLoadProgress {
 	}
 
 	override fun onFailed(failure: IApi) {
-		mToLoad.remove(failure)
+		Log.d(UtilityLoader::class.simpleName, "Remove: ${failure.getUrl()}")
+		mToLoad.remove(failure.getUrl())
+		Log.d(UtilityLoader::class.simpleName, "Failure: ${failure.getUrl()}")
 		mListeners.forEach {
 			it.onFailed(failure)
 		}
